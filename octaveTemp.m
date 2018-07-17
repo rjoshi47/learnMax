5cf2d017a1ee06cd1d2ec003721067dbec040840
Rahul Joshi <35663367+rjoshi47@users.noreply.github.com>
2018-07-17 15:07:45 +0530

    Create octaveTemp.m

---
+++ octaveTemp.m
@@ -0,0 +1,10 @@
+X_norm = X;
+m = length(X_norm);
+mu = zeros(1, size(X_norm, 2));
+sigma = zeros(1, size(X_norm, 2));
+mu = mean(X_norm);
+sigma = std(X_norm);
+X_norm = X_norm - repmat(mu, m, 1);
+sigma = sigma.^(-1);
+sigma2 = [sigma(1,1) 0; 0 sigma(1,2)]
+X_norm = X_norm*sigma2;
